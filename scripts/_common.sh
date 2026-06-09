6532ded850484aabfa791c202302eefcb5f67770
Leszek <lpryszcz@crg.es>
2026-06-09 11:47:31 +0200

    scripts/restore

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,11 @@
 # get corpus from wiki
 generate_corpus(){
     for i in `seq 1 1 50`; do
-	w3m -t 1 -s https://simple.wikipedia.org/wiki/Special:Random > corpus.txt;
+	w3m -t 1 -s https://simple.wikipedia.org/wiki/Special:Random | grep -vP '^ |^\[|●|•' > corpus.txt;
 	s=$(grep -n "From Simple English Wikipedia, the free encyclopedia" corpus.txt|cut -f1 -d:);
 	e=$(grep -n 'Retrieved from "https://simple.wikipedia.org/w/index.php' corpus.txt|cut -f1 -d:);
-	head -n $(expr $e - 5) corpus.txt|tail +$(expr $s + 2)|sed 's/•//g;' >> $install_dir/mishmash.txt;
+	head -n $(expr $e - 5) corpus.txt|tail +$(expr $s + 2) >> $install_dir/mishmash.txt;
     done
+    # store new hash
+    sha256sum $install_dir/mishmash.txt | awk '{print toupper($1)}' > $install_dir/seed.txt
 }
